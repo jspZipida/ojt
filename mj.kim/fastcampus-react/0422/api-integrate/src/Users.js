55fc2e3f07c959c4ab1f463790eedb28cce491fc
MJun-Kim <81725244+MJun-Kim@users.noreply.github.com>
2021-04-22 18:47:02 +0900

    Add react-native practice project and review-react (#36)

---
+++ mj.kim/fastcampus-react/0422/api-integrate/src/Users.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useAsync } from "react-async";
+import User from "./User";
+import { getUsers, useUsersDispatch, useUsersState } from "./UsersContext";
+
+function Users() {
+  const [userId, setUserId] = useState(null);
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  const { loading, data: users, error } = state.users;
+
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
+
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id} onClick={() => setUserId(user.id)}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchData}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </div>
+  );
+}
+
+export default Users;

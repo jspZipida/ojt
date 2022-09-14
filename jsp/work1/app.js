8c0e94ab254a77b3537747f4e245ab6e92a710e2
jspZipida <jsp@zipida.com>
2022-09-14 10:57:32 +0900

    push work1 done

--- jsp/work1/app.js
+++ jsp/work1/app.js
@@ -1,5 +1,5 @@
-const root = document.getElementById('root');
 
+const root = document.getElementById('root');
 const addBtn = document.querySelector('#button1'); 
 const insertItemText = document.querySelector('#itemText'); 
 const todoList = document.querySelector('#list'); 
@@ -14,34 +14,21 @@ function insertItem(text){
   item.push(sendParam);
   localStorage.setItem('item', JSON.stringify(item));
   sendParam = {};
-  selectList();
+  drawList(text);
+  insertItemText.value = "";
 }
 
 function reloadPage(){
 
 }
 
-
 function selectList(){
-
   var getList = JSON.parse(localStorage.getItem('item'));
-
   if(getList === null){
     return false;
   } else{
       getList.forEach(function(item){
-        // drawList(item.text);
-        const li = document.createElement('li');
-        const span = document.createElement('span');
-        const deleteBtn = document.createElement('button');
-        
-        span.innerHTML = item.text;
-        deleteBtn.innerText = 'Del';
-        deleteBtn.id = 'delBtn';
-
-        li.appendChild(span); // li에 text넣어놓은 span추가
-        li.appendChild(deleteBtn); // li에 deleteBtn추가
-        todoList.appendChild(li); // 리스트 추가
+        drawList(item.text);
       });
   }
   item = getList;
@@ -56,16 +43,35 @@ function drawList(item){
   span.innerHTML = item;
   deleteBtn.innerText = 'Del';
   deleteBtn.id = 'delBtn';
-  // deleteBtn.addEventListener('click', toDoDelete); // deleteBtn click Event
-  li.appendChild(span); // li에 text넣어놓은 span추가
-  li.appendChild(deleteBtn); // li에 deleteBtn추가
-  todoList.appendChild(li); // 리스트 추가
+  deleteBtn.onclick = deleteList;
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn); 
+  todoList.appendChild(li); 
+}
+
+function deleteList(param){
+  var getList = JSON.parse(localStorage.getItem('item'));
+  let deleteValue = param.target.parentNode; //li 추적
+  const list = document.querySelectorAll('li');
+  const arrayList = [...list]; // 깊은 복사 해야지 index tracking 가능
+  
+  let indexOfTarget = arrayList.indexOf(deleteValue);
+
+  // html li 초기화
+  for(let i=0;i<list.length;i++){   
+    list[i].remove();
+  }
+
+  // li 새로 그리기
+  getList.splice(indexOfTarget, 1);
+  item = getList; 
+  localStorage.setItem('item', JSON.stringify(item));
+  selectList();
 }
 
 function initPage(){
   selectList();
 }
-
-// localStorage.clear();
 
 initPage();
